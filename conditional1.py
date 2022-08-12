ade4ce6b899b7729f3a7c002e681a6e874e22486
Palanikumar K <palanikumar1284@gmail.com>
2022-08-12 15:26:50 +0530

    Add a new line and commit

--- conditional1.py
+++ conditional1.py
@@ -9,4 +9,4 @@ elif Age>18:
     print("You are Eligible to Vote,", Name, 'and', "Your Age is:",Age)
     print("You are not Eligible to vote in this Election")
 else:
-    print("Invalid Request from user")
+    print("Invalid Request from user 1")
